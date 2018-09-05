aa7ee5ed5cc576925b6017eb903042ddafdf55ae
薛晴 <setsuhare@setsuharematoMacBook-Pro.local>
2018-09-05 23:05:08 +0800

    12

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -20,13 +20,20 @@ Page({
     var incommingsoonurl = app.globalData.doubanBase + '/v2/movie/coming_soon' + '?start=0&count=3';
     var inthearterurl = app.globalData.doubanBase + '/v2/movie/in_theaters' + '?start=0&count=3';
 
-    this.getrequestlist(inthearterurl, 'inthearter');
+    this.getrequestlist(inthearterurl, 'inthearter', '最近上映');
 
-    this.getrequestlist(top250url, 'top250');
-    this.getrequestlist(incommingsoonurl, 'commingsoon');
+    this.getrequestlist(top250url, 'top250', '全部250名');
+    this.getrequestlist(incommingsoonurl, 'commingsoon', '即将上映');
 
   },
-  getrequestlist: function(url, setkey) {
+
+  onmoretap: function() {
+    wx.navigateTo({
+      url: 'more-movies/more-movies'
+
+    })
+  },
+  getrequestlist: function(url, setkey, setkey1) {
     var that = this;
     wx.request({
       // 豆瓣API需要使用http://t.yushu.im代替之前的https://api.douban.com
@@ -38,19 +45,14 @@ Page({
       dataType: 'json',
       responseType: 'text',
       success: function(res) {
-        // console.log(res.data);
-        that.processDouBanData(res.data, setkey)
+        that.processDouBanData(res.data, setkey, setkey1)
 
       },
-      //  failed: function (res) {
-      //   console.log(res.data);
-      //   // that.processDouBanData(res.data)
 
-      // }
     })
 
   },
-  processDouBanData: function(moviedouban, setkey) {
+  processDouBanData: function(moviedouban, setkey, setkey1) {
     var movies = [];
     for (var idx in moviedouban.subjects) {
       var subject = moviedouban.subjects[idx];
@@ -60,7 +62,7 @@ Page({
       }
       var image = subject.images.large;
       var rating = subject.rating.average;
-       var stars = util.convertstar1(subject.rating.stars);
+      var stars = util.convertstar1(subject.rating.stars);
       // console.log(subject.rating.stars);
       var temp = {
         image: image,
@@ -72,9 +74,13 @@ Page({
       movies.push(temp);
     }
     // 添加JS对象是为了让setkey与movies一一对应上，{ movies: movies}是为了方便模板movieslist直接调用。
+    // 不能直接使用将movies: movies传入到data的原因是：1.三行显示无法分辨去对应的movie,只会读取出同一个movies
+    //  2.读取完三个获取数据的函数后，Movies被更新为最后一个读取数据的Movies
     var readysetkey = {};
     readysetkey[setkey] = {
-      movies: movies
+      movies: movies,
+      setkey: setkey1
+
     };
     this.setData(readysetkey);
 
--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,7 @@
 function convertstar(stars) {
   var star = [];
   var num = stars.toString().substring(0, 1);
-  console.log(num);
+  // console.log(num);
   for (var i=1; i <= 5; i++) {
     if (i <= num) {
       star.push(1);
